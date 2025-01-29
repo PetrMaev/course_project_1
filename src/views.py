aa8a0586f45a0d8931708e215c1b342223047c72
Petr Maev <petr.maev94@gmail.com>
2025-01-29 22:57:57 +0300

    add update functions and add view function

--- src/views.py
+++ src/views.py
@@ -1,18 +1,19 @@
 import json
 import os
 from datetime import datetime, time
+from typing import Any
 
 import pandas as pd
 import requests
 from dotenv import load_dotenv
 
-from config import PATH_TO_OPERATIONS
+from config import PATH_TO_OPERATIONS, PATH_TO_USER_SETTINGS
 
 load_dotenv()
 
 
 def greeting() -> str:
-    """ Функция приветствия, в зависимости от текущего времени """
+    """ Возвращает приветствие, в зависимости от текущего времени """
     time_now = datetime.now().time()
     if time(hour=23) < time_now <= time(hour=23, minute=59, second=59) or time(hour=0) < time_now <= time(hour=6):
         return 'Доброй ночи'
@@ -24,78 +25,78 @@ def greeting() -> str:
         return 'Доброе утро'
 
 
-def cards_info(df_transactions: pd.DataFrame) -> str:
+def cards_info(set_time: str) -> Any:
     """ Возвращает информацию по каждой карте """
-    df_transactions_sort = df_transactions.loc[df_transactions['Сумма платежа'] < 0]
+    df_transactions = pd.read_excel(PATH_TO_OPERATIONS)
+    df_transactions_sort = df_transactions.loc[
+        (df_transactions['Сумма платежа'] < 0) & (df_transactions['Дата платежа'] == set_time)]
     cashback = df_transactions_sort['Сумма операции с округлением'] / 100
     df_transactions['Кешбэк'] = cashback
     grouped_df = df_transactions.groupby('Номер карты').agg(
         {'Сумма операции с округлением': 'sum', 'Кешбэк': 'sum'}).reset_index()
     result = grouped_df.loc[:, ['Номер карты', 'Сумма операции с округлением', 'Кешбэк']]
-    result_to_dict = result.to_dict(orient='records')
-    return json.dumps(result_to_dict, ensure_ascii=False, indent=4)
+    result_dict = result.to_dict(orient='records')
+    return result_dict
 
 
-def top_transactions(df_transactions: pd.DataFrame, pay_amount: int | float) -> str:
+def top_transactions(set_time: str) -> Any:
     """ Возвращает топ 5 транзакций по сумме платежа """
-    sorted_df = df_transactions.loc[df_transactions['Сумма операции с округлением'] < pay_amount]
-    top_5 = sorted_df.sort_values(by='Сумма операции с округлением', ascending=False).head()
+    df_transactions = pd.read_excel(PATH_TO_OPERATIONS)
+    df_transactions_sort = df_transactions.loc[
+        (df_transactions['Сумма платежа'] < 0) & (df_transactions['Дата платежа'] == set_time)]
+    top_5 = df_transactions_sort.sort_values(by='Сумма операции с округлением', ascending=False).head()
     top_transactions_sort = top_5.loc[:, ['Дата платежа', 'Сумма операции с округлением', 'Категория', 'Описание']]
     top_5_dict = top_transactions_sort.to_dict(orient='records')
-    return json.dumps(top_5_dict, ensure_ascii=False, indent=4)
+    return top_5_dict
 
 
-def get_exchange_rate() -> str:
+def get_exchange_rate(path_to_user_file: str) -> list:
     """ Получение актуальных данных курса валют """
-    # Получение курса доллара
-    payload = {"to": "RUB", "from": "USD"}
-    get_api = os.getenv("API_KEY_CURRENCIES")
-    headers = {"apikey": f"{get_api}"}
-    url = f"https://api.apilayer.com/exchangerates_data/convert?&amount=1"
-    response = requests.get(url, headers=headers, params=payload)
-    response.raise_for_status()
-    currency_usd = response.json()['query']['from']
-    rate_usd = response.json()['info']['rate']
-    # Получение курса евро
-    payload = {"to": "RUB", "from": "EUR"}
-    get_api = os.getenv("API_KEY_CURRENCIES")
-    headers = {"apikey": f"{get_api}"}
-    url = f"https://api.apilayer.com/exchangerates_data/convert?&amount=1"
-    response = requests.get(url, headers=headers, params=payload)
-    response.raise_for_status()
-    currency_eur = response.json()['query']['from']
-    rate_eur = response.json()['info']['rate']
-    # Вывод результата
-    result = dict()
-    result["currency_rates"] = {"currency": currency_usd, "rate": rate_usd}, {"currency": currency_eur,
-                                                                              "rate": rate_eur},
-    return json.dumps(result, ensure_ascii=False, indent=4)
+    currency_list = []
+    with open(path_to_user_file) as user_file:
+        data = json.load(user_file)
+    for currency in data['user_currencies']:
+        currency_dict = {}
+        payload = {"to": "RUB", "from": f"{currency}"}
+        get_api = os.getenv("API_KEY_CURRENCIES")
+        headers = {"apikey": f"{get_api}"}
+        url = "https://api.apilayer.com/exchangerates_data/convert?&amount=1"
+        response = requests.get(url, headers=headers, params=payload)
+        response.raise_for_status()
+        currency_dict['currency'] = response.json()['query']['from']
+        currency_dict['rate'] = round(response.json()['info']['rate'], 2)
+        currency_list.append(currency_dict)
+    return currency_list
 
 
-def get_stock_price() -> str:
+def get_stock_price(path_to_user_file: str) -> list:
     """ Получение актуальных данных стоимости акций """
-    stock = "AAPL"
-    get_api = os.getenv("API_KEY_STOCKS")
-    url = f"https://financialmodelingprep.com/api/v3/quote-short/{stock}?apikey={get_api}"
-    response = requests.get(url)
-    response.raise_for_status()
-    stock_name = response.json()[0]['symbol']
-    stock_price = round(response.json()[0]['price'], 2)
-    result = dict()
-    result["stock_prices"] = {"stock": stock_name, "price": stock_price}
-    return json.dumps(result, ensure_ascii=False, indent=4)
+    stocks_list = []
+    with open(path_to_user_file) as user_file:
+        data = json.load(user_file)
+    for stock in data['user_stocks']:
+        stocks_dict = {}
+        get_api = os.getenv("API_KEY_STOCKS")
+        url = f"https://financialmodelingprep.com/api/v3/quote-short/{stock}?apikey={get_api}"
+        response = requests.get(url)
+        response.raise_for_status()
+        stocks_dict["stock"] = response.json()[0]['symbol']
+        stocks_dict["price"] = round(response.json()[0]['price'], 2)
+        stocks_list.append(stocks_dict)
+    return stocks_list
 
 
-def views():
+def views(set_time: str) -> str:
     """ Собирает все данные и возвращает JSON-объект """
-    pass
-
+    views_dict = {'greeting': greeting(), 'cards': cards_info(set_time),
+                  'top_transactions': top_transactions(set_time),
+                  'currency_rates': get_exchange_rate(PATH_TO_USER_SETTINGS),
+                  'stock_prices': get_stock_price(PATH_TO_USER_SETTINGS)}
+    return json.dumps(views_dict, ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
     print(greeting())
-    df_operations = pd.read_excel(PATH_TO_OPERATIONS)
-    # print(top_transactions(df_operations, 800))
-    # print(cards_info(df_operations))
-    # print(get_exchange_rate())
-    # print(get_stock_price())
+    print(views('25.12.2021'))
+    # print(cards_info('25.12.2021'))
+    # print(top_transactions('25.12.2021'))
