62b6c0a8beef488b116934ce26a56045201ae99c
Petr Maev <petr.maev94@gmail.com>
2025-01-30 22:39:44 +0300

    add a logger for the functions of the utils

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ from pathlib import Path
 PATH = Path(__file__).parent
 PATH_TO_OPERATIONS = PATH / 'data' / 'operations.xlsx'
 PATH_TO_USER_SETTINGS = PATH / 'user_settings.json'
+PATH_TO_UTILS_LOGS = PATH / 'logs' / 'utils.log'
