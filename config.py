d0c3dc7ab5945880ec32c044367eeb1b322028bf
Petr Maev <petr.maev94@gmail.com>
2025-02-01 19:51:03 +0300

    add services.py and functional

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ PATH = Path(__file__).parent
 PATH_TO_OPERATIONS = PATH / 'data' / 'operations.xlsx'
 PATH_TO_USER_SETTINGS = PATH / 'user_settings.json'
 PATH_TO_UTILS_LOGS = PATH / 'logs' / 'utils.log'
+PATH_TO_SERVICES_LOGS = PATH / 'logs' / 'services.log'
